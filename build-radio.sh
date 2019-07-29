4a55ff7bbbd210fdf0321a3bfa5260a36e52934d
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2019-07-29 15:58:06 +1200

    Add script to build a gnuradio+extras setup the way I like it.

---
+++ build-radio.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+export SOURCE_BASE=~/autobuild/src/
+export BUILD_BASE=~/autobuild/base/
+
+mkdir -p "$SOURCE_BASE"
+mkdir -p "$BUILD_BASE"
+
+cd "$SOURCE_BASE"
+git clone --recurse-submodules https://www.gnuradio.org/cgit/gnuradio.git/
+cd gnuradio
+mkdir build
+cd build
+cmake -DPYTHON-EXECUTABLE=/usr/bin/python3 -DCMAKE_INSTALL_PREFIX="$BUILD_BASE" -DENABLE_GRC=ON -DENABLE_GR_UHD=ON -DENABLE_GR_QTGUI=ON ../ && make && make test && make install
+if [ $? -ne 0 ]
+then
+  exit -1
+fi
+
+cd "$SOURCE_BASE"
+git clone https://github.com/bastibl/gr-foo.git -b master --single-branch
+cd gr-foo
+mkdir build
+cd build
+cmake -DPYTHON-EXECUTABLE=/usr/bin/python3 -DCMAKE_INSTALL_PREFIX="$BUILD_BASE" ../ && make && make install
+if [ $? -ne 0 ]
+then
+  exit -1
+fi
+
+cd "$SOURCE_BASE"
+git clone https://github.com/bastibl/gr-ieee802-11.git -b master --single-branch
+cd gr-ieee802-11
+mkdir build
+cd build
+cmake -DPYTHON-EXECUTABLE=/usr/bin/python3 -DCMAKE_INSTALL_PREFIX="$BUILD_BASE" ../ && make && make install
+if [ $? -ne 0 ]
+then
+  exit -1
+fi
+
